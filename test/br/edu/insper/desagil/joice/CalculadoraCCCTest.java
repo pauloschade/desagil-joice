0665a995a251bb8b98d841f9392e1012edf26fc5
jvpazotti <62957238+jvpazotti@users.noreply.github.com>
2021-03-27 11:39:21 -0300

    teste CCC finalizado

--- test/br/edu/insper/desagil/joice/CalculadoraCCCTest.java
+++ test/br/edu/insper/desagil/joice/CalculadoraCCCTest.java
@@ -1,6 +1,9 @@
 package br.edu.insper.desagil.joice;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class CalculadoraCCCTest {
 	
@@ -8,14 +11,23 @@ public class CalculadoraCCCTest {
 	private double  densidade;
 	private static double DELTA=0.05;
 	
-	@BeforeEach
-	public void setUp() {
-		calculatorCCC= new CalculdadoraCCC();
-	}
 	
 	@Test
 	public void testNiobio() {
-		densidade=calculatorCCC.calcula()
+		densidade=calculatorCCC.calcula(92.91,0.143);
+		assertEquals(8.57,densidade,DELTA);
+	}
+	
+	@Test
+	public void testTungstenio() {
+		densidade=calculatorCCC.calcula(183.84,0.137);
+		assertEquals(19.28,densidade,DELTA);
+	}
+	
+	@Test
+	public void testLitio() {
+		densidade=calculatorCCC.calcula(6.94,0.152);
+		assertEquals(0.53,densidade,DELTA);
 	}
 	
 }
