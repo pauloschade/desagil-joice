b833395a9ed8fee881a4c31b656fad3e700f05f9
jvpazotti <62957238+jvpazotti@users.noreply.github.com>
2021-03-28 18:25:42 -0300

    ajustei o src e test

--- test/br/edu/insper/desagil/joice/model/CalculadoraCCCTest.java
+++ test/br/edu/insper/desagil/joice/model/CalculadoraCCCTest.java
@@ -11,24 +11,26 @@ public class CalculadoraCCCTest {
 	private double  densidade;
 	private static double DELTA=0.05;
 	
+	@BeforeEach
+	void setUp() {
+		calculatorCCC = new CalculadoraCCC();
+	}
+	
 	
 	@Test
 	public void testNiobio() {
-		calculatorCCC=new CalculadoraCCC("Niobio");
 		densidade=calculatorCCC.calcula(92.91,0.143);
 		assertEquals(8.57,densidade,DELTA);
 	}
 	
 	@Test
 	public void testTungstenio() {
-		calculatorCCC=new CalculadoraCCC("Tungstenio");
 		densidade=calculatorCCC.calcula(183.84,0.137);
 		assertEquals(19.28,densidade,DELTA);
 	}
 	
 	@Test
 	public void testLitio() {
-		calculatorCCC=new CalculadoraCCC("Litio");
 		densidade=calculatorCCC.calcula(6.94,0.152);
 		assertEquals(0.53,densidade,DELTA);
 	}
