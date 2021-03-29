a38ea4ac95a2f6e20f1ac445da34aad4b4949f06
jvpazotti <62957238+jvpazotti@users.noreply.github.com>
2021-03-28 21:05:42 -0300

    arrumei o cálculo do método calcula CCC

--- src/br/edu/insper/desagil/joice/model/CalculadoraCCC.java
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCCC.java
@@ -9,10 +9,11 @@ public class CalculadoraCCC extends Calculadora {
 	public double calcula(double peso,double raio) {
 		double numeroAvogadro = 6.02214 * Math.pow(10,23);
 		double raioCm = raio/10000000;
-		double massaAtomica = (2 * peso)/numeroAvogadro;
-		double numerador = (2) * peso;
+		double massaAtomica = (4 * peso)/numeroAvogadro;
+		double aresta = (4*raioCm)/(Math.pow(3, 0.5));
+		double volume = Math.pow(aresta, 3);
 		double denominador = Math.pow(((4 * raio) / Math.pow(3, 0.5)),3);
-		return numerador/denominador;
+		return massaAtomica/volume;
 	}
 
 }
