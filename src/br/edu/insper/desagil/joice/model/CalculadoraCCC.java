6ac4fc7047d7b5440a9ea0523b272e1bbcf488b6
jvpazotti <62957238+jvpazotti@users.noreply.github.com>
2021-03-29 12:37:05 -0300

    deixei o calculo fazer mais sentido com o que fizemos em aula

--- src/br/edu/insper/desagil/joice/model/CalculadoraCCC.java
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCCC.java
@@ -9,10 +9,10 @@ public class CalculadoraCCC extends Calculadora {
 	public double calcula(double peso,double raio) {
 		double numeroAvogadro = 6.02214 * Math.pow(10,23);
 		double raioCm = raio/10000000;
-		double massaAtomica = (4 * peso)/numeroAvogadro;
+		double massaAtomica = (4* peso)/numeroAvogadro;
 		double aresta = (4*raioCm)/(Math.pow(3, 0.5));
-		double volume = Math.pow(aresta, 3);
-		return (massaAtomica/volume)/2;
+		double volume =2*Math.pow(aresta, 3);
+		return (massaAtomica/volume);
 	}
 
 }
